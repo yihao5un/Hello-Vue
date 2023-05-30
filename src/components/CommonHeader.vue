4b746bad5ac68c6d0afa7be9e57062f5ad9ccffc
yihao.sun <yihao.sun@accenture.com>
2023-05-31 00:21:20 +0800

    [FEAT] CommonHeader组件 & 样式

---
+++ src/components/CommonHeader.vue
@@ -0,0 +1,58 @@
+<template>
+    <div class="header-container">
+        <div class="l-content">
+            <el-button icon="el-icon-menu" size="mini"></el-button>
+            <!-- 面包屑 -->
+            <span class="text">首页</span>
+        </div>
+        <div class="r-content">
+            <el-dropdown>
+                <span class="el-dropdown-link">
+                    <img class="user" src="../assets/images/user.png" alt="">
+                </span>
+                <el-dropdown-menu slot="dropdown">
+                    <el-dropdown-item>个人中心</el-dropdown-item>
+                    <el-dropdown-item>退出</el-dropdown-item>
+                </el-dropdown-menu>
+            </el-dropdown>
+        </div>
+    </div>
+</template>
+<script>
+export default {
+    data() {
+        return {
+
+        }
+    }
+}
+</script>
+<style lang="less" scoped>
+.header-container {
+    padding: 0 20px;
+    background-color: #333;
+    height: 60px;
+    // display:flex 意思是弹性布局，它能够扩展和收缩 flex 容器内的元素，以最大限度地填充可用空间
+    display: flex;
+    // 两侧居中
+    justify-content: space-between;
+    // 垂直居中
+    align-items: center;
+
+    // 面包屑样式
+    .text {
+        color: #fff;
+        font-size: 14px;
+        margin-left: 10px;
+    }
+
+    // 图片样式
+    .r-content {
+        .user {
+            width: 40px;
+            height: 40px;
+            border-radius: 50%;
+        }
+    }
+}
+</style>
--- src/views/Main.vue
+++ src/views/Main.vue
@@ -6,7 +6,10 @@
                 <common-aside />
             </el-aside>
             <el-container>
-                <el-header>Header</el-header>
+                <el-header>
+                    <!-- 用小驼峰命名下面引用的组件 -->
+                    <common-header />
+                </el-header>
                 <el-main>
                     <h1>我是Main</h1>
                     <!-- 子路由出口 -->
@@ -19,15 +22,22 @@
 <script>
 // 导入组件
 import CommonAside from '../components/CommonAside.vue'
+import CommonHeader from '../components/CommonHeader.vue'
 export default {
     data() {
         return {
-            
+
         }
     },
     // 引入组件
     components: {
-        CommonAside
+        CommonAside,
+        CommonHeader
     }
 }
 </script>
+<style scoped>
+.el-header {
+    padding: 0;
+}
+</style>
