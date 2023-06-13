e84e057ec157612cf53dcf61cc6cea9de096c487
yihao.sun <yihao.sun@accenture.com>
2023-06-13 17:44:33 +0800

    [feat]: Home 购买统计部分

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -14,6 +14,22 @@
                     <p>上次登录地点: <span>武汉</span></p>
                 </div>
             </el-card>
+            <el-card style="margin-top: 20px;">
+                <el-table :data="tableData" style="width: 100%">
+                    <!-- <el-table-column prop="name" label="课程">
+                    </el-table-column>
+                    <el-table-column prop="todayBuy" label="今日购买">
+                    </el-table-column>
+                    <el-table-column prop="monthBuy" label="本月购买">
+                    </el-table-column>
+                    <el-table-column prop="totalBuy" label="总购买">
+                    </el-table-column> -->
+
+                    <!-- 可以定义一个标签tableLabel去遍历 -->
+                    <!-- :prop :label 是动态属性 -->
+                    <el-table-column v-for="(val, key) in tableLabel" :prop="key" :label="val" />
+                </el-table>
+            </el-card>
         </el-col>
         <el-col :span="16">
             <div class="grid-content bg-purple-light"></div>
@@ -25,7 +41,50 @@
 export default {
     data() {
         return {
-
+            tableData: [
+                {
+                    name: 'oppo',
+                    todayBuy: 100,
+                    monthBuy: 300,
+                    totalBuy: 800
+                },
+                {
+                    name: 'vivo',
+                    todayBuy: 100,
+                    monthBuy: 300,
+                    totalBuy: 800
+                },
+                {
+                    name: '苹果',
+                    todayBuy: 100,
+                    monthBuy: 300,
+                    totalBuy: 800
+                },
+                {
+                    name: '小米',
+                    todayBuy: 100,
+                    monthBuy: 300,
+                    totalBuy: 800
+                },
+                {
+                    name: '三星',
+                    todayBuy: 100,
+                    monthBuy: 300,
+                    totalBuy: 800
+                },
+                {
+                    name: '魅族',
+                    todayBuy: 100,
+                    monthBuy: 300,
+                    totalBuy: 800
+                }
+            ],
+            tableLabel: {
+                name: "课程",
+                todayBuy: "今日购买",
+                monthBuy: "本月购买",
+                totalBuy: "总购买"
+            }
         }
     }
 }
