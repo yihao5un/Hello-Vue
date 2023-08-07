1345f52dfeee6bcb3e41e931c3542664f53a7ee4
yihao.sun <yihao.sun@accenture.com>
2023-08-07 16:25:04 +0800

    [feat]: 用户管理页面form编写

--- src/views/User.vue
+++ src/views/User.vue
@@ -1,32 +1,32 @@
 <template>
     <div class="manage">
-        <el-dialog title="提示" :visible.sync="dialogVisible" width="50%">
+        <el-dialog title="提示" :visible.sync="dialogVisible" width="50%" :before-close="handleClose">
             <!-- 用户的表单信息 -->
             <!-- :inline 一行 -->
-            <el-form ref="form" :inline="true" :model="form" label-widch="80px">
-                <el-form-item label="姓名">
+            <el-form ref="form" :rules="rules" :inline="true" :model="form" label-width="80px">
+                <el-form-item label="姓名" prop="name">
                     <el-input placeholder="请输入姓名" v-model="form.name"></el-input>
                 </el-form-item>
-                <el-form-item label="年龄">
+                <el-form-item label="年龄" prop="age">
                     <el-input placeholder="请输入年龄" v-model="form.age"></el-input>
                 </el-form-item>
-                <el-form-item label="性别">
+                <el-form-item label="性别" prop="sex">
                     <el-select v-model="form.sex" placeholder="请输入">
                         <el-option label="男" value="1"></el-option>
                         <el-option label="女" value="0"></el-option>
                     </el-select>
                 </el-form-item>
-                <el-form-item label="出生日期">
+                <el-form-item label="出生日期" prop="birth">
                     <el-date-picker v-model="form.birth" type="date" placeholder="选择日期">
                     </el-date-picker>
                 </el-form-item>
-                <el-form-item label="地址">
+                <el-form-item label="地址" prop="addr">
                     <el-input placeholder="请输入地址" v-model="form.addr"></el-input>
                 </el-form-item>
             </el-form>
             <span slot="footer" class="dialog-footer">
-                <el-button @click="dialogVisible = false">取 消</el-button>
-                <el-button type="primary" @click="dialogVisible = false">确 定</el-button>
+                <el-button @click="cancel">取 消</el-button>
+                <el-button type="primary" @click="submit">确 定</el-button>
             </span>
         </el-dialog>
         <div class="manage-header">
@@ -47,7 +47,65 @@ export default {
                 sex: '',
                 birth: '',
                 addr: ''
+            },
+            rules: {
+                name: [
+                    {
+                        required: true,
+                        message: '请输入姓名'
+                    }
+                ],
+                age: [
+                    {
+                        required: true,
+                        message: '请输入年龄'
+                    }
+                ],
+                sex: [
+                    {
+                        required: true,
+                        message: '请选择性别'
+                    }
+                ],
+                birth: [
+                    {
+                        required: true,
+                        message: '请选择出生日期'
+                    }
+                ],
+                addr: [
+                    {
+                        required: true,
+                        message: '请输入地址'
+                    }
+                ]
             }
+        }
+    },
+    methods: {
+        // 提交用户表单
+        submit() {
+            this.$refs.form.validate((valid) => {
+                if (valid) {
+                    // 后续对表单数据的获取
+                    console.log(this.form, 'form')
+                    this.$refs.form.resetFields()
+                    // 关闭弹窗
+                    this.dialogVisible = false
+
+                }
+            })
+        },
+        // 弹窗关闭的时候
+        handleClose() {
+            this.$refs.form.resetFields()
+            // 关闭 dialog
+            this.dialogVisible = false
+        },
+        // 点击按钮的时候
+        cancel() {
+            // 调用方法
+            this.handleClose()
         }
     }
 }
